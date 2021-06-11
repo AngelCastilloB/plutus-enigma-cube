f6d13fd81e9f254f3c46a6b25a5cf122895b7367
Angel Castillo <angel.castillob@protonmail.com>
2021-06-11 22:16:37 +0800

    Added the cube parameters of the state machine

--- src/Enigma/OnChain.hs
+++ src/Enigma/OnChain.hs
@@ -1,18 +1,66 @@
--- 
---  Copyright (c) 2021 Angel Castillo.
--- 
---  Licensed under the Apache License, Version 2.0 (the "License");
---  you may not use this file except in compliance with the License.
---  You may obtain a copy of the License at
--- 
---    http://www.apache.org/licenses/LICENSE-2.0
--- 
---  Unless required by applicable law or agreed to in writing, software
---  distributed under the License is distributed on an "AS IS" BASIS,
---  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
---  See the License for the specific language governing permissions and
---  limitations under the License.
---
+{-|
+Module      : OnChain
+Description : Contains the contract code that goes in the blockchain.
+Copyright   : (c) 2021 Angel Castillo
+License     : Apache-2.0
+Maintainer  : angel.castillob@protonmail.com
+Stability   : experimental
+
+Onchain contract code of the Enigma Cube game on the cardano blockchain.
+-}
+
+-- LANGUAGE EXTENSIONS --------------------------------------------------------
+
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+
+-- MODULE DEFINITION ----------------------------------------------------------
 
 module Enigma.OnChain(
     ) where
+
+-- IMPORTS --------------------------------------------------------------------
+
+import           Control.Monad                hiding (fmap)
+import           Data.Aeson                   (FromJSON, ToJSON)
+import           Data.Text                    (Text, pack)
+import           GHC.Generics                 (Generic)
+import           Plutus.Contract              as Contract hiding (when)
+import           Plutus.Contract.StateMachine
+import qualified PlutusTx
+import           PlutusTx.Prelude             hiding (Semigroup(..), check, unless)
+import           Ledger                       hiding (singleton)
+import           Ledger.Ada                   as Ada
+import           Ledger.Constraints           as Constraints
+import           Ledger.Typed.Tx
+import qualified Ledger.Typed.Scripts         as Scripts
+import           Ledger.Value
+import           Playground.Contract          (ToSchema)
+import           Prelude                      (Semigroup (..))
+import qualified Prelude
+
+-- DATA TYPES -----------------------------------------------------------------
+
+data CubeParameter = CubeParameter
+    { cubeId             :: !AssetClass
+    , solvedPuzzles      :: !Integer
+    , stateMachineNft    :: !AssetClass
+    , firstReward        :: !AssetClass
+    , secondReward       :: !AssetClass
+    , thirdReward        :: !AssetClass
+    , lastReards         :: !AssetClass
+    } deriving (Show, Generic, FromJSON, ToJSON, Prelude.Eq, Prelude.Ord)
+
+PlutusTx.makeLift ''CubeParameter
+
+-- DEFINITIONS ----------------------------------------------------------------
